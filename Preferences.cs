79e4b09a0bca25ea8bc9cc4eeb8589cb82247055
Philip Chu <bitbucket@technicat.com>
2017-08-26 23:03:28 -0700

    command to clear player prefs

---
+++ Preferences.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+
+namespace Fugu.Editor {
+
+
+	public class Preferences {
+
+		[MenuItem ("FuguGames/Preferences/Clear")]
+		static void Clear() {
+			PlayerPrefs.DeleteAll();
+		}
+	}
+}
